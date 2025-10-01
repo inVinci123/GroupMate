5f95d8555721e9b6befa70f17cdba846c49b59d0
Trisha Patel <pateltrisha2705@gmail.com>
2025-10-02 09:52:38 +1000

    modified basic start code

--- bot/bot.js
+++ bot/bot.js
@@ -1,23 +1,10 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+require("dotenv").config();
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ]
-});
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once('ready', () => {
+client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
-});
-
-client.on('messageCreate', (message) => {
-  if (message.author.bot) return;
-  if (message.content === '!ping') {
-    message.reply('Pong!');
-  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
