a1937d3ac38a85ffd437caabaec2c875ef4f95ac
Trisha Patel <pateltrisha2705@gmail.com>
2025-10-02 12:18:26 +1000

    changed

--- bot/deploy-commands.js
+++ bot/deploy-commands.js
@@ -1,45 +1,31 @@
+
 import { REST, Routes, SlashCommandBuilder } from "discord.js";
 import dotenv from "dotenv";
-
 dotenv.config();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
 
 const commands = [
   new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!"),
-  new SlashCommandBuilder()
-    .setName("newproject")
-    .setDescription("Create a new project")
-    .addStringOption(option => 
-      option.setName("name").setDescription("Project name").setRequired(true)
-    ),
-  new SlashCommandBuilder()
-    .setName("addtask")
-    .setDescription("Add a task to a project")
-    .addStringOption(option =>
-      option.setName("task").setDescription("Task name").setRequired(true)
-    )
-    .addStringOption(option =>
-      option.setName("due").setDescription("Due date").setRequired(true)
-    )
-]
-.map(command => command.toJSON());
+].map(c => c.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
-const CLIENT_ID = 1422842708279693422;
-const GUILD_ID = 1422845854091579454;
-
 (async () => {
   try {
-    console.log("Started refreshing application (/) commands.");
+    console.log("Deploying commands...");
+    console.log("CLIENT_ID:", CLIENT_ID);
+    console.log("GUILD_ID:", GUILD_ID);
 
     await rest.put(
       Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
       { body: commands }
     );
 
-    console.log("Successfully reloaded application (/) commands.");
+    console.log("Commands deployed successfully!");
   } catch (error) {
     console.error(error);
   }
