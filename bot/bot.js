5cf7ff4d4499eeaa111c6fd974fee579bae14c5c
Trisha Patel <pateltrisha2705@gmail.com>
2025-10-02 11:34:56 +1000

    starter code for bot

--- bot/bot.js
+++ bot/bot.js
@@ -1,13 +1,13 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.MessageContent
   ],
 });
 
@@ -17,22 +17,9 @@ client.once("ready", () => {
 
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
-  if (!message.content.startsWith("!")) return;
 
-  const [cmd, ...args] = message.content.slice(1).split(" ");
-
-  if (cmd === "ping") {
+  if (message.content === "!ping") {
     message.reply("Pong!");
-  }
-
-  if (cmd === "newproject") {
-    const name = args.join(" ");
-    message.channel.send(`Project created: **${name}**`);
-  }
-
-  if (cmd === "addtask") {
-    const [task, due] = args;
-    message.channel.send(`Task added: **${task}** due on ${due}`);
   }
 });
 
